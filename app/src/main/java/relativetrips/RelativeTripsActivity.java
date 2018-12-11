80df446d5c7b700ee0fdde6524d8bbdfd306bbf2
IoanaCosmineanu <cosmineanu.ioana@gmail.com>
2018-12-11 09:18:56 +0200

    init

---
+++ app/src/main/java/relativetrips/RelativeTripsActivity.java
@@ -0,0 +1,15 @@
+package relativetrips;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ioana.traveljournal.R;
+
+public class RelativeTripsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_trips);
+    }
+}
